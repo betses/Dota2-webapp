d9f871b6c5958e6b03f753c3a17cd65f654f975e
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-10-06 19:26:51 +0300

    Fix linter error

--- src/tests/Main.test.js
+++ src/tests/Main.test.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
 import Main from '../components/Main';
-import store from '../redux/configureStore';
+import store from '../Redux/configureStore';
 
 jest.mock('axios');
 
